5b4d94402c3f1c46a6b837e17be5871dceae2918
Informatica <informatica@alumno.local>
2024-02-26 16:18:47 -0600

    Creacion completa de el servidor y mongoDB

---
+++ config/server.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { dbConnection } from './morgo.js';
+import authRoutes from '../src/auth/auth.routes.js'
+
+
+class server{
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        //this.adminPath = '/'
+        //this.authPath = '/'
+
+        this.middlewares();
+        this.connectDB();
+        this.routes();
+    }
+
+        async connectDB() {
+        await dbConnection();
+        }
+
+    middlewares(){
+    this.app.use(express.urlencoded({ extended: false }));
+    }
+
+    routes() {
+        this.app.use(this.adminPath, adminRoutes);
+        this.app.use(this.authPath, authRoutes)
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Server runnig on port ', this.port);
+        });
+    }
+
+}
+
+export default Server;

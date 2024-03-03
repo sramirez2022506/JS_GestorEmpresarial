f2bed9cfe0fe8e40e2f082c471a2236cc098b797
sramirez-2022506 <sramirez-2022506@kinal.edu.gt>
2024-03-03 17:02:46 -0600

    The company routes has been updated

--- src/Company/company.routes.js
+++ src/Company/company.routes.js
@@ -1,4 +1,42 @@
 import {Router} from 'express';
 import {check} from 'express-validator';
 import {validateFields} from '../middlewares/validate-fields.js';
-import {validateJWT} from '../middlewares/validate-jwt.js'
+import {validateJWT} from '../middlewares/validate-jwt.js';
+
+import {
+    companyPost,
+    companyGet,
+    companyPut,
+    reportExcel
+} from '../Company/company.controller.js'
+
+const router = Router();
+
+router.get("/", companyGet)
+
+router.get("/report", reportExcel)
+
+router.post("/new",
+[
+    validateJWT,
+    check("nCompany", "The name of the company is obligatory"),
+    check("tarjectory", "The trajectory is obligatory"),
+    check("levelImpact", "The level impact is obligatory"),
+    check("category", "The category is obligatory"),
+    validateFields,
+], companyPost);
+
+router.put(
+    "/:id",
+    [
+        validateJWT,
+        check("nCompany", "The name of the company is obligatory"),
+        check("tarjectory", "The trajectory is obligatory"),
+        check("levelImpact", "The level impact is obligatory"),
+        check("category", "The category is obligatory"),
+        validateFields,
+    ],
+    companyPut
+);
+
+export default router;

69c4637ad362c6869189c794c8514ac13a8326c9
sramirez-2022506 <sramirez-2022506@kinal.edu.gt>
2024-03-03 16:17:03 -0600

    companyModel has been updated

--- config/server.js
+++ config/server.js
@@ -3,19 +3,19 @@ import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import { dbConnection } from './mongo.js';
-//import authRoutes from '../src/auth/auth.routes.js'
+import authRoutes from '../src/auth/auth.routes.js'
 
 
 class Server{
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        //this.adminPath = '/'
-        //this.authPath = '/'
+        this.adminPath = '/'
+        this.authPath = '/'
 
         this.middlewares();
         this.connectDB();
-        //this.routes();
+        this.routes();
     }
 
         async connectDB() {
@@ -26,10 +26,10 @@ class Server{
     this.app.use(express.urlencoded({ extended: false }));
     }
 
-    /*routes() {
+    routes() {
         this.app.use(this.adminPath, adminRoutes);
         this.app.use(this.authPath, authRoutes)
-    }*/
+    }
 
     listen() {
         this.app.listen(this.port, () => {
---
+++ src/Company/company.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+
+const companySchema = mongoose.Schema({
+    nCompany:{
+        type: String,
+        required: [true, "The company name is obligatory"]
+    },
+
+    trajectory: {
+        type: String,
+        required: [true, "The trajectory is obligatory"]
+    },
+
+    levelImpact: {
+        type: String,
+        required: [true, "The impcat level is obligatory"]
+    },
+
+    category: {
+        type: Boolean,
+        default: true
+    }
+});
+
+companySchema.methods.joJson = function(){
+    const {__v,_id,...company} = this.toObject();
+    company.uid = _id;
+    return company;
+}
+
+export default mongoose.model("company", companySchema);

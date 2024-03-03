cb1bab717881ef02506db4e8623e4aef10d52667
sramirez-2022506 <sramirez-2022506@kinal.edu.gt>
2024-03-03 15:43:37 -0600

    authRoutes has been updated

---
+++ src/auth/auth.routes.js
@@ -0,0 +1,19 @@
+import {Router} from 'express';
+import {check} from 'express-validator';
+
+import {login} from './auth.controller.js';
+import {validateField} from '../middlewares/validate-fields.js';
+
+const router = Router();
+
+router.post(
+    "/login",
+    [
+        check("email", "The email is obligatory").isEmail(),
+        check("password", "the password is obligatory").not().isEmpty(),
+        validateField
+    ],
+    login
+);
+
+export default router

fbd321f974ed698e7595221fcf38f9864cf02164
sramirez-2022506 <sramirez-2022506@kinal.edu.gt>
2024-03-03 15:38:34 -0600

    authController has been updated

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,3 +1,37 @@
 import bcryptjs from 'bcryptjs';
 import Administrator from '../Admin/admin.model.js';
-import {generateJWT} from "../helpers/generate-jwt.hs";
+import {generateJWT} from '../helpers/generate-jwt.js';
+
+export const login = async (req, res) =>{
+    const {email, password} = req.body;
+
+    try{
+        const admin = await Administrator.findOne({email});
+
+        if(!admin){
+            return res.status(400).json({
+                msg: "Incorrect credentials"
+            });
+        }
+
+        const validateKey = bcryptjs.compareSync(password, admin.password);
+        if(!validateKey){
+            return res.status(400).json({
+                msg: "Incorrect password"
+            });
+        }
+
+        const token = await generateJWT(admin.id);
+
+        res.status(200).json({
+            msg: "Welcome!!!",
+            admin,
+            token
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            msg: "Please communicate with the admin"
+        });
+    }
+}
